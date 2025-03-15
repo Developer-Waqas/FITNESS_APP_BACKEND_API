b32a834860688bc4bd5cb35e8957f764e0574fa2
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-03-15 19:36:44 +0500

    Register API

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const authRouter = express.Router();
-
 authRouter.post('/api/register', async (req, res) => {
     try {
         const { fullName, phoneNumber, email, password } = req.body;
@@ -10,10 +10,11 @@ authRouter.post('/api/register', async (req, res) => {
         if (existUser) {
             return res.status(400).json({ message: "User with same Email/Phone Number already exists" });
         } else {
-            const user = new User({ fullName, phoneNumber, email, password });
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            const user = new User({ fullName, phoneNumber, email, password: hashedPassword });
             const saveUser = await user.save();
-
-            res.status(200).json({ data: saveUser, message: "User Created Successfully!" });
+            res.status(200).json({ message: "User Created Successfully!", data: { saveUser } });
         }
     } catch (error) {
         res.status(500).json({ message: error.message });
