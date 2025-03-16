96a5391390a16b668cfbab93e5a5705479d07bc7
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-03-16 08:15:51 +0500

    login api

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 const authRouter = express.Router();
+
+
+// Register API
 authRouter.post('/api/register', async (req, res) => {
     try {
         const { fullName, phoneNumber, email, password } = req.body;
@@ -19,6 +23,30 @@ authRouter.post('/api/register', async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
+});
+
+// Login API
+authRouter.post('/api/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const findUser = await findOne({ email });
+        if (!findUser) {
+            return res.status(400).json({ message: "User not found with this Email!" })
+        } else {
+            const isMatch = await bcrypt.compare(password, findUser.password);
+            if (!isMatch) {
+                return res.status(400).json({ message: "Incorrect Password!" });
+            } else {
+                const token = jwt.sign({ id: findUser._id }, "PasswordKey");
+                const { password, ...userWithoutPassword } = findUser._doc;
+                res.status(200).json({ message: "User Login Successfully!" }, { token, ...userWithoutPassword });
+            }
+        }
+    } catch (error) {
+
+    }
+
+
 });
 
 module.exports = authRouter;
